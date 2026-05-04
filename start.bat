27ea64bce58a293de43919fee3572988550d34a1
Ivan Chaev <mega.yannov@gmail.com>
2026-05-04 10:10:09 +0300

    Update and rename TTranscribe.bat to start.bat

--- start.bat
+++ start.bat
@@ -4,8 +4,10 @@ where pythonw >nul 2>nul
 if %errorlevel% neq 0 (
     echo Pythonw.exe не найден в системном PATH.
     echo Убедитесь, что Python установлен и добавлен в переменную PATH, затем повторите запуск.
+    echo Если Python установлен в нестандартную папку, откройте этот bat-файл в Блокноте
+    echo и замените "pythonw.exe" на полный путь, например: C:\МояПапка\pythonw.exe
     pause
     exit /b 1
 )
-start "" pythonw.exe transcriber.py
+start "" pythonw.exe transcriber_tts.py
 exit
